9e8c22e74e6e8fcc6c2ffb5d4f2414efdd82c078
John Buxton <lqueryvg@gmail.com>
2024-12-02 21:26:14 +0000

    day 1 done

---
+++ day01/part1.py
@@ -0,0 +1,23 @@
+
+# f = 'day01/input-example.txt'
+f = 'day01/input.txt'
+
+def main(lines):
+  left = []
+  right = []
+
+  for line in lines:
+    a = list(map(int, line))
+    left.append(a[0])
+    right.append(a[1])
+  
+  total = 0
+  for l, r in zip(sorted(left), sorted(right)):
+    total += abs(l - r)
+
+  print(total)
+
+with open(f) as f:
+  lines = f.readlines()
+  lines = [line.strip().split() for line in lines]
+  main(lines)
---
+++ day01/part2.py
@@ -0,0 +1,32 @@
+
+# f = 'day01/input-example.txt'
+f = 'day01/input.txt'
+
+def main(lines):
+  left = []
+  right = []
+
+  for line in lines:
+    a = list(map(int, line))
+    left.append(a[0])
+    right.append(a[1])
+
+  counts = {}
+
+  for r in right:
+    if r in counts:
+      counts[r] += 1
+    else:
+      counts[r] = 1
+  
+  total = 0
+  for l in left:
+    if l in counts:
+      total += l * counts[l]
+
+  print(total)
+
+with open(f) as f:
+  lines = f.readlines()
+  lines = [line.strip().split() for line in lines]
+  main(lines)
