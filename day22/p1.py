d07f502381a1d66243fbb85815580392a6618fca
John Buxton <lqueryvg@gmail.com>
2024-12-22 19:59:03 +0000

    day 22 part 1

---
+++ day22/p1.py
@@ -0,0 +1,32 @@
+f = 'input-tiny.txt'
+f = 'input-example.txt'
+f = 'input.txt'
+# ITERATIONS = 10
+ITERATIONS = 2000
+def evolve(num):
+  # print(f"{num=}")
+  # num * 64
+  num = ((num * 64) ^ num) % 16777216
+  num = ((num // 32) ^ num) % 16777216
+  num = ((num * 2048) ^ num) % 16777216
+  return num
+
+def go(nums):
+  total = 0
+  for num in nums:
+    new = num
+    for i in range(0, ITERATIONS):
+      new = evolve(new)
+    print(f"{num=}, {new=}")
+    total += new
+  return total
+
+def main():
+  global f, lines
+  with open(f) as f:
+    lines = f.readlines()
+    nums = [int(line.strip()) for line in lines]
+    return go(nums)
+
+if __name__ == '__main__':
+  print(main())
